e831cf232418f5cd38b999b84a011621b8313a47
Luka Weis <34310346+Businesskasper@users.noreply.github.com>
2022-10-14 15:45:22 +0200

    Update HashtableAsObject.ps1

--- Misc/HashtableAsObject.ps1
+++ Misc/HashtableAsObject.ps1
@@ -1,6 +1,44 @@
 # Converts a hashtable to a pscustomobject
 # Example: $greeting = @{ hello = 'world' } | HashtableAsObject
 
+function HashtableAsObject() {
+
+    [cmdletbinding()]
+    param(
+        [parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        $item
+    )
+
+    # Convert children
+    if ($item -is [Hashtable] -or $item -is [System.Collections.Generic.Dictionary[string, object]]) {
+        $copy = [PSCustomObject]::new()
+        $item.GetEnumerator() | % {
+            $enumerable = $_
+            $copy | Add-Member -MemberType NoteProperty -Name $_.Key -Value (ConvertToPsObject -item $_.Value)
+        }
+        return $copy
+    }
+    elseif ($item -is [PSCustomObject]) {
+        $copy = [PSCustomObject]::new()
+        $item | Get-Member -MemberType NoteProperty | % {
+            $member = $_
+            $copy | Add-Member -MemberType NoteProperty -Name $_.Name -Value (ConvertToPsObject -item $item.($_.Name))
+        }
+        return $copy
+    }
+    elseif ([System.Collections.Generic.IEnumerable[object]].IsAssignableFrom($item.GetType())) {
+        $copy = [PSCustomObject[]]@()
+        foreach ($child in $item) {
+            $copy += ConvertToPsObject -item $child
+        }
+        return $copy
+    }
+    else {
+        return $item
+    }
+}
+
+<#
 function HashtableAsObject {
 
     [cmdletbinding()]
@@ -16,3 +54,4 @@ function HashtableAsObject {
 
     return $obj
 }
+#>
