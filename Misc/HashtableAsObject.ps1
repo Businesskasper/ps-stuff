35a2c66738acc205f405eb577a152fa40d4b5396
Luka Weis <34310346+Businesskasper@users.noreply.github.com>
2024-04-18 11:42:17 +0200

    Update HashtableAsObject.ps1

--- Misc/HashtableAsObject.ps1
+++ Misc/HashtableAsObject.ps1
@@ -2,36 +2,58 @@
 # Example: $greeting = @{ hello = 'world' } | HashtableAsObject
 
 function HashtableAsObject() {
-
     [cmdletbinding()]
     param(
+        [AllowNull()]
         [parameter(Mandatory = $true, ValueFromPipeline = $true)]
         $item
     )
+    if ($null -eq $item) {
+        return $null
+    }
 
-    # Convert children
     if ($item -is [Hashtable] -or $item -is [System.Collections.Generic.Dictionary[string, object]]) {
+        # item is hashtable or object
+        # create object and recursively add members
         $copy = [PSCustomObject]::new()
-        $item.GetEnumerator() | % {
-            $copy | Add-Member -MemberType NoteProperty -Name $_.Key -Value (ConvertToPsObject -item $_.Value)
+        $members = $item.GetEnumerator()
+        foreach ($member in $members) {
+            $asObj = toObject -item $member.Value
+            $copy | Add-Member -MemberType NoteProperty -Name $member.Key -Value $asObj
         }
         return $copy
     }
+    <#
+    # Temporary workaround since for some reason a String was recognized as PSCustomObject in the next clause
+    elseif ($item.GetType().Name -eq "String" -or $item.GetType().Name -like "Int*" -or $item.GetType().Name -eq "Boolean") {
+        # item is a primitive value
+        # return as is
+        return $item
+    }
+    #>
     elseif ($item -is [PSCustomObject]) {
+        # item is an object
+        # copy object and recursively add members
         $copy = [PSCustomObject]::new()
-        $item | Get-Member -MemberType NoteProperty | % {
-            $copy | Add-Member -MemberType NoteProperty -Name $_.Name -Value (ConvertToPsObject -item $item.($_.Name))
+        $members = $item | Get-Member -MemberType NoteProperty
+        foreach ($member in $members) {
+            $asObj = toObject -item $item.($member.Name)
+            $copy | Add-Member -MemberType NoteProperty -Name $member.Name -Value $asObj
         }
         return $copy
     }
     elseif ([System.Collections.Generic.IEnumerable[object]].IsAssignableFrom($item.GetType())) {
+        # item is a collection
+        # create new array and recursviely add members
         $copy = [PSCustomObject[]]@()
-        foreach ($child in $item) {
-            $copy += ConvertToPsObject -item $child
+        foreach ($member in $item) {
+            $copy += toObject -item $member
         }
         return $copy
     }
     else {
+        # item is a primitive value
+        # return as is
         return $item
     }
 }
